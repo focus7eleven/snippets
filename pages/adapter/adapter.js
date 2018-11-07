50f9fe0cd389d4ba7e2edd2b7e0e076bc8ba52cb
focus7eleven <441002295@qq.com>
2018-11-07 10:58:30 +0800

    Adapter - call

--- pages/adapter/adapter.js
+++ pages/adapter/adapter.js
@@ -11,3 +11,18 @@ const ary = (fn, n) => (...args) => fn(...args.slice(0, n))
 const firstTwoMax = ary(Math.max, 2);
 const res = [[7, 6, 9], [8, 4, 6], [10]].map(x => firstTwoMax(...x)); // [6, 8, 10]
 console.log('res : ', res );
+
+/**
+ * call
+ * Given a key and a set of arguments, call them when given a context. Primarily useful in composition.
+ */
+
+const call = (key, ...args) => context => context[key](...args)
+
+Promise.resolve([1, 2, 3])
+  .then(call('map', x => 2 * x))
+  .then(console.log); // [ 2, 4, 6 ]
+const map = call.bind(null, 'map');
+Promise.resolve([1, 2, 3])
+  .then(map(x => 2 * x))
+  .then(console.log); // [ 2, 4, 6 ]
