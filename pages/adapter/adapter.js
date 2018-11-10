d641a3343a6dc80fccd09a8685be660827643f38
longcha <441002295@qq.com>
2018-11-10 18:17:02 +0800

    Adataper - collectInto

--- pages/adapter/adapter.js
+++ pages/adapter/adapter.js
@@ -19,6 +19,7 @@ console.log('res : ', res );
 
 const call = (key, ...args) => context => context[key](...args)
 
+// example
 Promise.resolve([1, 2, 3])
   .then(call('map', x => 2 * x))
   .then(console.log); // [ 2, 4, 6 ]
@@ -26,3 +27,18 @@ const map = call.bind(null, 'map');
 Promise.resolve([1, 2, 3])
   .then(map(x => 2 * x))
   .then(console.log); // [ 2, 4, 6 ]
+
+
+/**
+ * collectInto
+ * Changes a function that accepts an array into a variadic function.
+ */
+
+const collectInto = func => (...args) => func(args)
+
+// example 
+const Pall = collectInto(Promise.all.bind(Promise));
+let p1 = Promise.resolve(1);
+let p2 = Promise.resolve(2);
+let p3 = new Promise(resolve => setTimeout(resolve, 2000, 3));
+Pall(p1, p2, p3).then(console.log); // [1, 2, 3] (after about 2 seconds)
