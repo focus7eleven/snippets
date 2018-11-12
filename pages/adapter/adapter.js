4868914fb00e6ffbb527a203d28312c1a4cca2f0
focus7eleven <441002295@qq.com>
2018-11-12 20:09:20 +0800

    add travis

--- pages/adapter/adapter.js
+++ pages/adapter/adapter.js
@@ -1,62 +1,33 @@
-/**
- * ary
- * Creates a function that accepts up to `n` arguments, ignoring any additional arguments.
- * fn: any function
- * n: up to `n` arguments
- */
+class Adapter {
+  /**
+   * ary
+   * Creates a function that accepts up to `n` arguments, ignoring any additional arguments.
+   * fn: any function
+   * n: up to `n` arguments
+   */
+  ary = (fn, n) => (...args) => fn(...args.slice(0, n))
 
-const ary = (fn, n) => (...args) => fn(...args.slice(0, n))
+  /**
+   * call
+   * Given a key and a set of arguments, call them when given a context. Primarily useful in composition.
+   */
+  call = (key, ...args) => context => context[key](...args)
 
-// example
-const firstTwoMax = ary(Math.max, 2);
-const res = [[7, 6, 9], [8, 4, 6], [10]].map(x => firstTwoMax(...x)); // [6, 8, 10]
-console.log('res : ', res );
+  /**
+   * collectInto
+   * Changes a function that accepts an array into a variadic function.
+   */
+  collectInto = func => (...args) => func(args)
 
-/**
- * call
- * Given a key and a set of arguments, call them when given a context. Primarily useful in composition.
- */
+  /**
+   * flip
+   * Flip takes a function as an argument, then makes the first argument the last.
+   */
+  flip = fn => (first, ...rest) => fn(...rest, first)
+}
 
-const call = (key, ...args) => context => context[key](...args)
+const adatper = new Adapter()
 
-// example
-Promise.resolve([1, 2, 3])
-  .then(call('map', x => 2 * x))
-  .then(console.log); // [ 2, 4, 6 ]
-const map = call.bind(null, 'map');
-Promise.resolve([1, 2, 3])
-  .then(map(x => 2 * x))
-  .then(console.log); // [ 2, 4, 6 ]
-
-
-/**
- * collectInto
- * Changes a function that accepts an array into a variadic function.
- */
-
-const collectInto = func => (...args) => func(args)
-
-// example 
-const Pall = collectInto(Promise.all.bind(Promise));
-let p1 = Promise.resolve(1);
-let p2 = Promise.resolve(2);
-let p3 = new Promise(resolve => setTimeout(resolve, 2000, 3));
-Pall(p1, p2, p3).then(console.log); // [1, 2, 3] (after about 2 seconds)
+export default adatper
 
 
-/**
- * flip
- * Flip takes a function as an argument, then makes the first argument the last.
- */
-
-const flip = fn => (first, ...rest) => fn(...rest, first)
-
-let a = { name: 'John Smith' };
-let b = {};
-const mergeFrom = flip(Object.assign);
-let mergePerson = mergeFrom.bind(null, a);
-const res1 = mergePerson(b); // == b
-b = {};
-const res2 = Object.assign(b, a); // == b
-console.log('res1 : ', res1 );
-console.log('res2 : ', res2 );
---
+++ test/adapter.test.js
@@ -0,0 +1,45 @@
+const adapter = require('../pages/adapter/adapter.js').default
+
+describe('test adapter', () => {
+  test('test ary', () => {
+    const firstTwoMax = adapter.ary(Math.max, 2)
+    const res = [[7, 6, 9], [8, 4, 6], [10]].map(x => firstTwoMax(...x))
+    expect(res).toEqual([7, 8, 10])
+  })
+
+  test('test call 1', () => {
+    Promise.resolve([1, 2, 3]).then(adapter.call('map', x => 2 * x)).then(res => {
+      expect(res).toEqual([2, 4, 6])
+    })
+  })
+
+  test('test call 2', () => {
+    const map = adapter.call.bind(null, 'map');
+    Promise.resolve([1, 2, 3]).then(map(x => 2 * x)).then(res => {
+      expect(res).toEqual([2, 4, 6])
+    })
+  })
+
+  test('test collect into', () => {
+    const Pall = adapter.collectInto(Promise.all.bind(Promise));
+    let p1 = Promise.resolve(1)
+    let p2 = Promise.resolve(2)
+    let p3 = new Promise(resolve => setTimeout(resolve, 2000, 3))
+    Pall(p1, p2, p3).then(res => {
+      expect(res).toEqual([1, 2, 3])
+    })
+  })
+
+  test('test flip', () => {
+    let a = { name: 'John Smith' };
+    let b = {};
+    const mergeFrom = adapter.flip(Object.assign);
+    let mergePerson = mergeFrom.bind(null, a);
+    const res1 = mergePerson(b); // == b
+    b = {};
+    const res2 = Object.assign(b, a); // == b
+
+    expect(res1).toEqual(a)
+    expect(res2).toEqual(a)
+  })
+});
